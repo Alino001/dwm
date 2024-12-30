cd73fc9471f92f04e478da806ad887551c3f98f9
Alino001 <118076662+Alino001@users.noreply.github.com>
2024-12-30 17:33:32 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,14 @@
+export root=/dev/sdb2
+export boot=/dev/sdb1
+export kernel=linux-zen
+export network=networkmanager
+export installtype=desktop
+export desktop=xfce
+export hostname=archlinux
+export timezone=Europe/Berlin
+export gpu=amd
+export username=adam
+export userpw=2012
+export rootpw=J2122010j
+export editor=nvim
+curl -L -s https://raw.githubusercontent.com/miguelrcborges/archinstallscript/main/start.sh | sh
